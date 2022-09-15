9673410600f11bb5f36cff571fa78a9f3b9646b1
Kehinde <adeniyekehinde0@gmail.com>
2022-09-14 21:38:30 -0700

    hh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
-#include "main.h"
+#include  "main.h"
 /**
- * main - Prints putchar as a message.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
