6689d27bd51305a365fb71f2353f2666edc01c5e
Kehinde <adeniyekehinde0@gmail.com>
2022-09-14 20:26:20 -0700

    fast

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef _main_h_
-#define _main_h_
+#define _main_h_ 
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,4 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endifi
+#endif
+
