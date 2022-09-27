799fc589e8b75fc5285bfe46a7b76f25458b0daf
damkenny <adeniyekehinde0@gmail.com>
2022-09-26 21:26:58 -0700

    0-memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
+
 char *_memcpy(char *dest, char *src, unsigned int n);
+
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
